8b8ae6794f4b03a02248b5f93abb99bd2781960e
tyrandev <andretyranowski@gmail.com>
2023-07-10 20:24:21 +0200

    register error message

--- src/main/java/com/example/keyboardmasters/controller/UserController.java
+++ src/main/java/com/example/keyboardmasters/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.keyboardmasters.controller;
 import com.example.keyboardmasters.model.User;
 import com.example.keyboardmasters.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,9 +31,15 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String register(User user) {
-        userService.save(user);
-        return "redirect:/login";
+    public String register(User user, Model model) {
+        try {
+            userService.save(user);
+            return "redirect:/login";
+        } catch (DataIntegrityViolationException ex) {
+            // add information to thymeleaf
+            model.addAttribute("registerErrorMessage", "Username already taken");
+            return "register";
+        }
     }
 
     @GetMapping("/login")
